58326a5598db91b075d5475437a630031a0ab940
Brian Deragon <bdderagon@infiniteenergy.com>
2016-06-02 09:55:25 -0400

    Added silent args

--- Droid4x/tools/chocolateyInstall.ps1
+++ Droid4x/tools/chocolateyInstall.ps1
@@ -3,6 +3,7 @@ $package = 'Droid4x'
 $params = @{
   PackageName = $package;
   FileType = 'exe';
+  SilentArgs = '/S';
   Url = "http://dl.haima.me/download/DXDown/win/Z001/Droid4XInstaller.exe";
 }
 Install-ChocolateyPackage @params
--- HipChat4/tools/chocolateyInstall.ps1
+++ HipChat4/tools/chocolateyInstall.ps1
@@ -3,6 +3,7 @@ $package = 'HipChat4'
 $params = @{
   PackageName = $package;
   FileType = 'exe';
+  SilentArgs = '/S'; 
   Url = "https://www.hipchat.com/downloads/latest/newqtwindows";
 }
 Install-ChocolateyPackage @params
